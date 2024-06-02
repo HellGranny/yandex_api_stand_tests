29044d2ab8228099b82342fa25a394ee28e0928e
HellGranny <mrs.torture@yandex.ru>
2024-06-02 18:21:23 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://78a93e2d-e65b-4b5c-9433-a35a5b9785fe.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "api/logs/main"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
